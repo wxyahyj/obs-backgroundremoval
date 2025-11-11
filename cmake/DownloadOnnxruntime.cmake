3c23cbb30d362168017ac53d982383a52ba47c54
Kaito Udagawa <umireon@kaito.tokyo>
2025-11-11 20:46:48 +0000

    Add GPU build for Windows (#703)
    * Add Windows GPU build and ONNX Runtime GPU option
    
    Introduces a new GitHub Actions workflow job for building the project on Windows with GPU support. Updates DownloadOnnxruntime.cmake to allow selecting between GPU and CPU ONNX Runtime downloads on Windows via a GPU option.
    
    * Update push.yaml
    
    * Automate artifact renaming and simplify DLL/PDB install
    
    Adds a step in the GitHub Actions workflow to rename the Windows x64 artifact with a '-gpu' suffix. Updates CMakeLists.txt to use globbing for installing all ONNX Runtime DLL and PDB files, simplifying the install command.
    
    * Update CMakeLists.txt

--- cmake/DownloadOnnxruntime.cmake
+++ cmake/DownloadOnnxruntime.cmake
@@ -10,6 +10,10 @@ if(NOT DEFINED PLATFORM)
   endif()
 endif()
 
+if(NOT DEFINED GPU)
+  set(GPU OFF)
+endif()
+
 file(REMOVE_RECURSE onnxruntime)
 
 if(PLATFORM STREQUAL "macos")
@@ -24,16 +28,29 @@ if(PLATFORM STREQUAL "macos")
   )
   file(RENAME onnxruntime-osx-universal2-1.23.2 onnxruntime)
 elseif(PLATFORM STREQUAL "windows")
-  file(
-    DOWNLOAD
-      https://github.com/microsoft/onnxruntime/releases/download/v1.23.2/onnxruntime-win-x64-gpu-1.23.2.zip
-      onnxruntime-win-x64-gpu-1.23.2.zip
-    EXPECTED_HASH SHA256=e77afdbbc2b8cb6da4e5a50d89841b48c44f3e47dce4fb87b15a2743786d0bb9
-  )
-  execute_process(
-    COMMAND ${CMAKE_COMMAND} -E tar xf onnxruntime-win-x64-gpu-1.23.2.zip
-  )
-  file(RENAME onnxruntime-win-x64-gpu-1.23.2 onnxruntime)
+  if(GPU)
+    file(
+      DOWNLOAD
+        https://github.com/microsoft/onnxruntime/releases/download/v1.23.2/onnxruntime-win-x64-gpu-1.23.2.zip
+        onnxruntime-win-x64-gpu-1.23.2.zip
+      EXPECTED_HASH SHA256=e77afdbbc2b8cb6da4e5a50d89841b48c44f3e47dce4fb87b15a2743786d0bb9
+    )
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -E tar xf onnxruntime-win-x64-gpu-1.23.2.zip
+    )
+    file(RENAME onnxruntime-win-x64-gpu-1.23.2 onnxruntime)
+  else()
+    file(
+      DOWNLOAD
+        https://github.com/microsoft/onnxruntime/releases/download/v1.23.2/onnxruntime-win-x64-1.23.2.zip
+        onnxruntime-win-x64-1.23.2.zip
+      EXPECTED_HASH SHA256=0b38df9af21834e41e73d602d90db5cb06dbd1ca618948b8f1d66d607ac9f3cd
+    )
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -E tar xf onnxruntime-win-x64-1.23.2.zip
+    )
+    file(RENAME onnxruntime-win-x64-1.23.2 onnxruntime)
+  endif()
 elseif(PLATFORM STREQUAL "linux")
   file(
     DOWNLOAD
