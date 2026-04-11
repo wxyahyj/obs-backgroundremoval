be11e5d99f823532fc4e59dbcc1118372b7c25c7
Pluto <3423377239@qq.com>
2026-04-11 15:54:13 +0800

    build(onnxruntime): 更新Linux平台ONNX Runtime下载版本至1.23.2
    添加对ONNX Runtime v1.23.2的下载支持，包含预期的SHA256校验和

--- cmake/DownloadOnnxruntime.cmake
+++ cmake/DownloadOnnxruntime.cmake
@@ -118,6 +118,18 @@ endif()
     message(STATUS "Downloaded ONNX Runtime CPU version")
   endif()
 elseif(PLATFORM STREQUAL "linux")
+  file(
+    DOWNLOAD
+      https://github.com/microsoft/onnxruntime/releases/download/v1.23.2/onnxruntime-linux-x64-1.23.2.tgz
+      onnxruntime-linux-x64-1.23.2.tgz
+    EXPECTED_HASH SHA256=1fa4dcaef22f6f7d5cd81b28c2800414350c10116f5fdd46a2160082551c5f9b
+  )
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} -E tar xf onnxruntime-linux-x64-1.23.2.tgz
+  )
+  file(RENAME onnxruntime-linux-x64-1.23.2 onnxruntime)
+  execute_process(COMMAND ln -s lib onnxruntime/lib64)
+endif()
 
 if(EXISTS onnxruntime/lib/cmake/onnxruntime/onnxruntimeTargets.cmake)
   file(READ onnxruntime/lib/cmake/onnxruntime/onnxruntimeTargets.cmake FILE_CONTENT)
